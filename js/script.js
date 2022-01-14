a79a3ece9fcf560c131f1f1100bb26bec99d5ade
arsenii_klinichev <arsenklinichev@gmail.com>
2022-01-14 23:52:42 +0300

    interactive elements desktop

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  // ymaps.ready(init);
+  // function init() {
+  //   var myMap = new ymaps.Map("map", {
+  //     center: [48.858821, 2.345705],
+  //     zoom: 12
+  //   }),
+
+  //     myPlacemark = new ymaps.Placemark([48.872185, 2.354224], {
+  //       balloonContent: 'X округ, улица дю Фобур Сен Дени 54'
+  //     }, {
+  //       iconLayout: 'default#image',
+  //       iconImageHref: 'img/placemark.svg',
+  //       iconImageSize: [28, 40],
+  //       iconImageOffset: [-15, -36]
+  //     });
+
+  //   myMap.geoObjects.add(myPlacemark);
+  // }
+
+  // const validation = new window.JustValidate('#form', {
+  //   errorLabelCssClass: 'form__label-invalid',
+  //   errorLabelStyle: {
+  //     color: '#ff5c00',
+  //   },
+  // });
+
+  // validation.addField('#name', [
+  //   {
+  //     rule: 'required',
+  //     errorMessage: 'Как вас зовут?',
+  //   },
+  // ])
+  // .addField('#tel', [
+  //   {
+  //     rule: 'required',
+  //     errorMessage: 'Укажите ваш телефон',
+  //   },
+  //   {
+  //     rule: 'minLength',
+  //     value: 10,
+  //     errorMessage: 'Номер указан не полностью',
+  //   },
+  // ])
+  // .addField('#email', [
+  //   {
+  //     rule: 'required',
+  //     errorMessage: 'Укажите ваш e-mail',
+  //   },
+  //   {
+  //     rule: 'email',
+  //     errorMessage: 'Указан некорректный e-mail',
+  //   },
+  // ]);
+
+  // var phoneSelector = document.querySelector("input[type='tel']");
+  // var im = new Inputmask("+7 (999) 999-99-99");
+  // im.mask(phoneSelector);
+});
